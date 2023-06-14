7a366f52ebe566a5be3454a20d40b59c4f47e5f7
Artemy Sychev L <artemsch17@gmail.com>
2023-06-14 21:18:10 +0500

    Changes

--- base.py
+++ base.py
@@ -1,32 +1,47 @@
 import base64
 import numpy as np
 from moviepy.editor import VideoFileClip
-from PIL import Image
+from PIL import Image, ImageDraw
 import os
 
-SAVING_FRAMES_PS = 2
+frames = np.empty(50, dtype=object)
 
-frames = set()
 
 def main(video, folder):
     c = 1
+    j = 0
     video_clip = VideoFileClip(video)
     file_name, _ = os.path.split(video)
+
+    x_size = int(input("Input x size of video: "))
+    y_size = int(input("Input y size of video: "))
 
     try:
         os.mkdir(folder)
     except:
-        print("Directory already exist")
+        print("Directory already exists")
 
-    step = 1 / SAVING_FRAMES_PS
+    step = 1
 
     for curr in np.arange(0, video_clip.duration, step):
-
-        filename = f"{folder}/Frame number {c}.jpg"
         frame = video_clip.get_frame(curr)
         img = Image.fromarray(frame)
-        img.save(filename)
+        draw = ImageDraw.Draw(img)
+        draw.rectangle((x_size - 270, y_size - 270, x_size, y_size), fill=(255, 255, 255))
+        frames[j] = base64.b64encode(img.tobytes())
+        j += 1
+        if (j == 20):
+            c = save_frames(folder, c)
+            j = 0
+
+
+def save_frames(folder, c):
+    for i in frames:
+        path = f"{folder}/Frame_number_{c}.jpg"
+        image_bytes = base64.b64decode(i)
+        image = Image.frombytes('RGB', (1280, 720), image_bytes)
+        image.save(path)
         c += 1
 
+main("math.mp4", "math") # First is name of file, seconnd is name of folder or directory("with /")
 
-main("math_lesson.mp4", "video")
